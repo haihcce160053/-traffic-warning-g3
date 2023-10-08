18d7a393a110191f74e41d944b47288f255141ea
Huynhhai42 <haihcce160053@fpt.edu.vn>
2023-10-08 10:08:26 +0700

    Intent login, signup, add more menu item

--- app/src/main/java/com/namhaigroup/map/MenuActivity.java
+++ app/src/main/java/com/namhaigroup/map/MenuActivity.java
@@ -1,14 +1,56 @@
 package com.namhaigroup.map;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
+    LinearLayout lnLogined, lnNotLogin;
+    Button btnNogSignIn, btnNotSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        lnLogined = findViewById(R.id.lnLogined);
+        lnNotLogin = findViewById(R.id.lnNotLogin);
+        btnNogSignIn = findViewById(R.id.btnNogSignIn);
+        btnNotSignUp = findViewById(R.id.btnNotSignUp);
+
+        btnNogSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(UserInformation.isLogin == false) {
+                    Intent i = new Intent(MenuActivity.this, LoginActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+        btnNotSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(UserInformation.isLogin == false) {
+                    Intent i = new Intent(MenuActivity.this, SignupActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(UserInformation.isLogin == true) {
+            lnLogined.setVisibility(View.VISIBLE);
+            lnNotLogin.setVisibility(View.GONE);
+        } else {
+            lnLogined.setVisibility(View.GONE);
+            lnNotLogin.setVisibility(View.VISIBLE);
+        }
     }
 }
---
+++ app/src/main/java/com/namhaigroup/map/UserInformation.java
@@ -0,0 +1,11 @@
+package com.namhaigroup.map;
+
+public class UserInformation {
+    public static boolean isLogin = false;
+    public static String premiumType;
+    public static String username;
+    public static String fullname;
+    public static String email;
+    public static String phone;
+    public static String address;
+}
